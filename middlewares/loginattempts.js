c16ba9df1c4d2bdac1ac03d120ad96c3c91dbdf2
LMY03 <86579817+LMY03@users.noreply.github.com>
2024-06-11 14:00:46 +0800

    Update loginattempts.js

--- middlewares/loginattempts.js
+++ middlewares/loginattempts.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
     if (!req.session.loginAttempts) req.session.loginAttempts = 0;
 
-    if (req.session.loginAttempts <= 3) {
+    if (req.session.loginAttempts >= 3) {
         req.flash('error', 'Too many login attempts. Please try again later.');
         return res.redirect('/');
     }
